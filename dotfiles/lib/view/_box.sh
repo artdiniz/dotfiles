82806e6a6ab13b1267fed601970c07220e38e367
Artur Diniz Adam <artur.daz@gmail.com>
2019-12-13 03:45:19 -0300

    Refactor box and clock_wise

--- dotfiles/lib/view/_box.sh
+++ dotfiles/lib/view/_box.sh
@@ -12,80 +12,57 @@ function _repeat_string {
 function _box {
     local _content="$1"
     local _box_chars="$2"
+    local _padding_sizes="$3"
 
-    local _horizontal_box_chars="${_box_chars:0:1}"
-    local _vertical_box_chars
-    test -z "${_box_chars:1:1}" \
-        && _vertical_box_chars="$_horizontal_box_chars" \
-        || _vertical_box_chars="${_box_chars:1:1}"
-    
-    local _padding_top_size _padding_right_size _padding_bottom_size _padding_left_size
-    _padding_top_size=0
-    _padding_right_size=0
-    _padding_bottom_size=0
-    _padding_left_size=0
+    local _top_box_char="–"
+    local _right_box_char="|"
+    local _bottom_box_char="–"
+    local _left_box_char="|"
 
-    if [ ! -z "$(echo "${@:3}")" ]; then
-        if [ -z "$(echo "${@:4}")" ]; then
-            _padding_size="$3"
-            _padding_top_size="$_padding_size"
-            _padding_right_size="$_padding_size"
-            _padding_bottom_size="$_padding_size"
-            _padding_left_size="$_padding_size"
-        elif [ -z "$(echo "${@:5}")" ]; then
-            _padding_vertical_size="$3"
-            _padding_horizontal_size="$4"
-            _padding_top_size="$_padding_vertical_size"
-            _padding_right_size="$_padding_horizontal_size"
-            _padding_bottom_size="$_padding_vertical_size"
-            _padding_left_size="$_padding_horizontal_size"
-        elif [ -z "$(echo "${@:6}")" ]; then
-            _padding_top_size="$3"
-            _padding_bottom_size="$5"
-            _padding_horizontal_size="$4"
-            _padding_right_size="$_padding_horizontal_size"
-            _padding_left_size="$_padding_horizontal_size"
-        else
-            _padding_top_size="$3"
-            _padding_right_size="$4"
-            _padding_bottom_size="$5"
-            _padding_left_size="$6"
-        fi
-    fi
+    _clock_wise_set_values "$_box_chars" _top_box_char _right_box_char _bottom_box_char _left_box_char
+
+    local _padding_top_size=0
+    local _padding_right_size=0
+    local _padding_bottom_size=0
+    local _padding_left_size=0
+
+    _clock_wise_set_values "$_padding_sizes" _padding_top_size _padding_right_size _padding_bottom_size _padding_left_size
 
     function _apply_line_padding {
         local _text="$1"
         local _padding_right="$(_repeat_string "$_padding_right_size")"
         local _padding_left="$(_repeat_string "$_padding_left_size")"
 
-        printf "$_vertical_box_chars$_padding_left$_text$_padding_right$_vertical_box_chars"
+        printf "%b%b%b%b%b" "$_left_box_char" "$_padding_left" "$_text" "$_padding_right" "$_right_box_char"
     }
 
     local _colum_count=0
-
     while IFS= read -r _content_line; do
         ((_colum_count<${#_content_line})) && { _colum_count="${#_content_line}"; }
     done <<<"$_content"
 
-    local _parsed_content=""
+    local _box_content=""
     while IFS= read -r _content_line; do
         local _current_line_count="${#_content_line}"
 
         local _padding_end_size=$((_colum_count - _current_line_count))
         local _padding_end="$(_repeat_string "$_padding_end_size")"
 
-        _parsed_content+="$(_apply_line_padding "$_content_line$_padding_end")\n"
+        _box_content+="$(_apply_line_padding "$_content_line$_padding_end")\n"
     done <<<"$_content"
- 
-    local _box_hr=" $(_repeat_string "$((_colum_count + _padding_right_size + _padding_left_size))" "$_horizontal_box_chars") \n"
-    
-    local _box_wr="$(_apply_line_padding "$(_repeat_string "$_colum_count")") \\\n"
-    local _box_padding_top="$(_repeat_string "$_padding_top_size" "$_box_wr")"
-    local _box_padding_bottom="$(_repeat_string "$_padding_bottom_size" "$_box_wr")"
 
-    printf "$_box_hr"
+    local _box_horizontal_borders_size=$((_colum_count + _padding_right_size + _padding_left_size))
+ 
+    local _box_top_border="$(_repeat_string "$_box_horizontal_borders_size" "$_top_box_char")"
+    local _box_bottom_border="$(_repeat_string "$_box_horizontal_borders_size" "$_bottom_box_char")"
+    
+    local _box_padding_line="$(_apply_line_padding "$(_repeat_string "$_colum_count")")\\\n"
+    local _box_padding_top="$(_repeat_string "$_padding_top_size" "$_box_padding_line")"
+    local _box_padding_bottom="$(_repeat_string "$_padding_bottom_size" "$_box_padding_line")"
+
+    printf " $_box_top_border \n"
     printf "$_box_padding_top"
-    printf "$_parsed_content"
+    printf "$_box_content"
     printf "$_box_padding_bottom"
-    printf "$_box_hr"
+    printf " $_box_bottom_border \n"
 }
--- dotfiles/lib/view/_clock_wise_set_values.sh
+++ dotfiles/lib/view/_clock_wise_set_values.sh
@@ -1,36 +1,45 @@
 function _clock_wise_set_values {
     local _param_values="$1"
 
+    local _top_value_var_name="$2"
+    local _right_value_var_name="$3"
+    local _bottom_value_var_name="$4"
+    local _left_value_var_name="$5"
+
     local _top_value _right_value _bottom_value _left_value
 
-    if [ ! -z "$(echo "${_param_values:0}")" ]; then
-        if [ -z "$(echo "${_param_values:1}")" ]; then
+    if [ ! -z "${_param_values:0}" ]; then
+        if [ -z "${_param_values:1}" ]; then
             _value="${_param_values:0:1}"
             _top_value="$_value"
             _right_value="$_value"
             _bottom_value="$_value"
             _left_value="$_value"
-        elif [ -z "$(echo "${_param_values:2}")" ]; then
+        elif [ -z "${_param_values:2}" ]; then
             _vertical_value="${_param_values:0:1}"
             _horizontal_value="${_param_values:1:1}"
-            
+
             _top_value="$_vertical_value"
             _right_value="$_horizontal_value"
             _bottom_value="$_vertical_value"
             _left_value="$_horizontal_value"
-        elif [ -z "$(echo "${_param_values:3}")" ]; then
+        elif [ -z "${_param_values:3}" ]; then
             _top_value="${_param_values:0:1}"
-            _bottom_value="${_param_values:2:2}"
-            
             _horizontal_value="${_param_values:1:1}"
+            _bottom_value="${_param_values:2:1}"
+            
             _right_value="$_horizontal_value"
             _left_value="$_horizontal_value"
         else
             _top_value="${_param_values:0:1}"
             _right_value="${_param_values:1:1}"
-            _bottom_value="${_param_values:2:2}"
-            _left_value="${_param_values:3:3}"
+            _bottom_value="${_param_values:2:1}"
+            _left_value="${_param_values:3:1}"
         fi
-    fi
 
+        read -r "$_top_value_var_name" <<< "$_top_value"
+        read -r "$_right_value_var_name" <<< "$_right_value"
+        read -r "$_bottom_value_var_name" <<< "$_bottom_value"
+        read -r "$_left_value_var_name" <<< "$_left_value"
+    fi
 }
