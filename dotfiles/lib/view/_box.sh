5b809c38e08449695ebca262499b0c86c491c863
Artur Diniz Adam <artur.daz@gmail.com>
2019-12-14 18:18:39 -0300

    Finish one dimension box

--- dotfiles/lib/view/_box.sh
+++ dotfiles/lib/view/_box.sh
@@ -11,7 +11,7 @@ function _repeat_string {
 
 function _box {
     local _content="$1"
-    local _box_strings="$2"
+    local _box_border_strings="$2"
     local _padding_sizes="$3"
 
     local _top_box_string="–"
@@ -19,7 +19,7 @@ function _box {
     local _bottom_box_string="–"
     local _left_box_string="|"
 
-    _clock_wise_set_values "$_box_strings" _top_box_string _right_box_string _bottom_box_string _left_box_string
+    _clock_wise_set_values "$_box_border_strings" _top_box_string _right_box_string _bottom_box_string _left_box_string
 
     local _padding_top_size=0
     local _padding_right_size=0
@@ -28,55 +28,69 @@ function _box {
 
     _clock_wise_set_values "$_padding_sizes" _padding_top_size _padding_right_size _padding_bottom_size _padding_left_size
 
-    function _apply_line_padding {
+    function _render_box_line {
         local _text="$1"
+        local _line_number="$2"
+        
+        local _current_left_box_char_position=$(( ($_line_number % ${#_left_box_string}) ))
+        local _current_left_box_char="${_left_box_string:$_current_left_box_char_position:1}"
+
+        local _current_right_box_char_position=$(( ($_line_number % ${#_right_box_string}) ))
+        local _current_right_box_char="${_right_box_string:$_current_right_box_char_position:1}"
+
         local _padding_right="$(_repeat_string "$_padding_right_size")"
         local _padding_left="$(_repeat_string "$_padding_left_size")"
 
-        printf "%b%b%b%b%b" "$_left_box_string" "$_padding_left" "$_text" "$_padding_right" "$_right_box_string"
+        printf "%b%b%b%b%b" "$_current_left_box_char" "$_padding_left" "$_text" "$_padding_right" "$_current_right_box_char"
     }
 
-    local _colum_count=0
+    # Get content dimensions
+    local _content_colum_count=0
+    local _content_lines_count=0
     while IFS=  read -r _content_line; do
-        ((_colum_count<${#_content_line})) && { _colum_count="${#_content_line}"; }
+        ((_content_colum_count<${#_content_line})) && { _content_colum_count="${#_content_line}"; }
+        _content_lines_count=$(( _content_lines_count + 1 ))
     done <<<"$_content"
 
     local _box_content=""
+    local _current_box_line_number=$_padding_top_size
     while IFS=  read -r _content_line; do
-        local _current_line_count="${#_content_line}"
+        local _current_line_column_count="${#_content_line}"
 
-        local _padding_end_size=$((_colum_count - _current_line_count))
+        local _padding_end_size=$((_content_colum_count - _current_line_column_count))
         local _padding_end="$(_repeat_string "$_padding_end_size")"
 
-        _box_content+="$(_apply_line_padding "$_content_line$_padding_end")\\n"
+        _box_content+="$(_render_box_line "$_content_line$_padding_end" "$_current_box_line_number")\\n"
+        _current_box_line_number=$((_current_box_line_number + 1))
     done <<<"$_content"
 
-    local _box_horizontal_borders_size=$((_colum_count + _padding_right_size + _padding_left_size))
+    local _box_horizontal_borders_size=$((_padding_left_size + _content_colum_count + _padding_right_size))
+    
+    local _box_top_border_repeat_number=$((($_box_horizontal_borders_size/${#_top_box_string})+1))
+    local _box_top_border="$(_repeat_string "$_box_top_border_repeat_number" "$_top_box_string")"
+    local _box_top_border=" ${_box_top_border:0:$_box_horizontal_borders_size}"
 
-    local _box_top_border=""
-    local i=0; while [ $i -lt ${#_top_box_string} ]; do
-        _box_top_border+="$(_repeat_string "${#_left_box_string}" " ")$(_repeat_string "$_box_horizontal_borders_size" "${_top_box_string:$i:1}")"
+    local _box_bottom_border_repeat_number=$((($_box_horizontal_borders_size/${#_bottom_box_string})+1))
+    local _box_bottom_border="$(_repeat_string "$_box_bottom_border_repeat_number" "$_bottom_box_string")"
+    local _box_bottom_border=" ${_box_bottom_border:0:$_box_horizontal_borders_size}"
+    
+    local _box_padding_top=""
+    local _box_padding_bottom=""
 
+    local _vertical_padding_filler="$(_repeat_string "$_content_colum_count")"
+    local _vertical_padding_height=$(($_padding_top_size + $_padding_bottom_size))
+    local i=0; while [ $i -lt "$_vertical_padding_height" ]; do
+        if [ $i -lt $_padding_top_size ]; then
+            _box_padding_top+="$(_render_box_line "$_vertical_padding_filler" "$i")\\n"
+        else
+            _box_padding_bottom+="$(_render_box_line "$_vertical_padding_filler" "$(($_content_lines_count + $i))")\\n"
+        fi
         i=$(( i + 1 ))
-        [ $i -lt ${#_top_box_string} ] && _box_top_border+="\n"
     done
 
-    local _box_bottom_border=""
-    local i=0; while [ $i -lt ${#_bottom_box_string} ]; do
-        _box_bottom_border+="$(_repeat_string "${#_left_box_string}" " ")$(_repeat_string "$_box_horizontal_borders_size" "${_bottom_box_string:$i:1}")"
-
-        i=$(( i + 1 ))
-        [ $i -lt ${#_bottom_box_string} ] && _box_bottom_border+="\n"
-    done
-    
-    local _box_padding_line="$(_apply_line_padding "$(_repeat_string "$_colum_count")")\\\n"
-    local _box_padding_top="$(_repeat_string "$_padding_top_size" "$_box_padding_line")"
-    local _box_padding_bottom="$(_repeat_string "$_padding_bottom_size" "$_box_padding_line")"
-    
-
-    printf "$_box_top_border \n"
-    printf "$_box_padding_top"
-    printf "$_box_content"
-    printf "$_box_padding_bottom"
-    printf "$_box_bottom_border \n"
+    printf "%b" "$_box_top_border \n"
+    printf "%b" "$_box_padding_top"
+    printf "%b" "$_box_content"
+    printf "%b" "$_box_padding_bottom"
+    printf "%b" "$_box_bottom_border \n"
 }
--- system/test_box.sh
+++ system/test_box.sh
@@ -1,4 +1,4 @@
-_texto =""
+_texto=""
 
 read -d '' -r _texto <<-ARQUIVO
 	Alô alô W Brasil
@@ -7,24 +7,18 @@ read -d '' -r _texto <<-ARQUIVO
 	Cuidado com o disco voador
 ARQUIVO
 
-printf "\n$(_style '$' $_text_red) $(_style '%s' $_bold)\n%s\n\n" \
-	'_box $_texto' \
-	"$(_box "$_texto")" \
-\
-	'_box "$_texto" – |" 1' \
-	"$(_box "$_texto" "– |" 1)"\
-\
-	'_box "$_texto" "*" "0 2"' \
-	"$(_box "$_texto" "*" "0 2")" \
-\
-	'_box "$_texto" "* |" "1 2 5"' \
-	"$(_box "$_texto" "* |" "1 2 5")" \
-\
-	'_box "$_texto" "* | •" "1 2 5 5"' \
-	"$(_box "$_texto" "* | •" "1 2 5 5")" \
-\
-	'_box "$_texto" "* « • »" "1 2"' \
-	"$(_box "$_texto" "* « • »" "1 2")" \
-\
-	'_box "$_texto" "_* || •+" "1 2 5 5"' \
-	"$(_box "$_texto" "_* || •–" "1 2 5 5")" \
+_commands=(
+	'_box "$_texto"'
+	'_box "$_texto" "– |" 1'
+	'_box "$_texto" "*" "0 2"'
+	'_box "$_texto" "* |" "1 2 5"'
+	'_box "$_texto" "* | •" "1 2 5 5"'
+	'_box "$_texto" "* « • »" "1 2"'
+	'_box "$_texto" "_* |•" "1 2 5 5"'
+	'_box "$_texto" "_* |•" "1 2 5 5"'
+	'_box "$_texto" "(≈ _*) (- •|) •+ (|- |•)" "1 2 5 5"'
+)
+
+for _command in "${_commands[@]}"; do
+	printf "\n$(_style "$" $_text_red) $(_style "%s" $_bold)\n\n%s\n\n\n" "'$_command'" "$(eval "$_command")"
+done
