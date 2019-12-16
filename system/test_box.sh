843ed74b7b674946f6d126772a89c348f1039636
Artur Diniz Adam <artur.daz@gmail.com>
2019-12-16 04:40:00 -0300

    More box tests

--- system/test_box.sh
+++ system/test_box.sh
@@ -9,6 +9,7 @@ ARQUIVO
 
 _commands=(
 	'_box "$_texto"'
+	'_box "$_texto" "" 1'
 	'_box "$_texto" "– |" 1'
 	'_box "$_texto" "2(_)(-) 2(|)(|) 2(–)(¯)" 1'
 	'_box "$_texto" "*" "0 2"'
@@ -25,22 +26,5 @@ _commands=(
 )
 
 for _command in "${_commands[@]}"; do
-	printf "\n$(_style "$" $_text_red) $(_style "%s" $_bold)\n\n%s\n\n\n" "'$_command'" "$(eval "$_command")"
+	printf "\n$(_style "$" $_text_red) $(_style "%s" $_bold)\n\n%s\n\n\n" "$_command" "$(eval "$_command")"
 done
-
-# $ _box "$_texto" "2(_)(_*) -•| 2(•+)(¯) 2(|-)(|•)" "1 2 5 5"
-#
-#       _________________________________
-#       _*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_ 
-#     ||                                 -
-#     -•     Alô alô W Brasil            •
-#     ||     Jacarezinho                 |
-#     -•     Avião                       -
-#     ||     Cuidado com o disco voador  •
-#     -•                                 |
-#     ||                                 -
-#     -•                                 •
-#     ||                                 |
-#     -•                                 -
-#       •+•+•+•+•+•+•+•+•+•+•+•+•+•+•+•+• 
-#       ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
