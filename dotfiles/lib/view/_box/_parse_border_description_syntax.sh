35cb14b40df5b1b52fa598a339819bf0f48b0a4d
artdinizpro16 <artdiniz@artdinizpro16.lan>
2020-07-12 04:39:53 -0300

    _box missing code for spaces as border chars

--- dotfiles/lib/view/_box/_parse_border_description_syntax.sh
+++ dotfiles/lib/view/_box/_parse_border_description_syntax.sh
@@ -5,11 +5,27 @@ function _parse_border_description_syntax {
 
 	local _parsed_borders _qt_borders
 
+	local _special_chars_by_alias
+	_create_string_var _special_chars_by_alias <<-SPECIAL_CHARS_BY_ALIAS
+		s 
+		t	
+	SPECIAL_CHARS_BY_ALIAS
+
+	while IFS= read -r _special_char_and_alias; do
+		local _alias="${_special_char_and_alias:0:1}"
+		local _char="${_special_char_and_alias:1:2}"
+
+		# echo "alias: '$_alias' || char '$_char'"
+		# _create_string_var _raw_border < <(printf %s "$_raw_border" | sed -e "s/\\\\$_alias/$_char/")
+		_create_string_var _raw_border < <(printf %s "${_raw_border/\\$_alias/$_char}")
+	done <<<"$(printf '%s' "$_special_chars_by_alias")"
+
+
 	# if _raw_border is a number (-eq only works for integers)
 	local first_border_char="${_raw_border:0:1}"
 	local second_border_char="${_raw_border:1:1}"
 	local last_border_char="${_raw_border:$(( ${#_raw_border} - 1 )):1}"
-	
+
 	if [ "$first_border_char" -eq "$first_border_char" ] 2> /dev/null && [ "$second_border_char" = "(" ] && [ "$last_border_char" = ")" ] ; then
 		_qt_borders=$first_border_char
 		local _borders_description=${_raw_border:1}	
@@ -23,18 +39,17 @@ function _parse_border_description_syntax {
 		local _STATE_READING_ESCAPE_NEXT=4
 		local _STATE_END_READING=5
 
-
-		local _previous_state=$_STATE_NOT_READING
 		function _set_state {
 			local _char="$1"
 			local _state_var_name="$2"
+			local _previous_state=${!_state_var_name}
 
 			local _state
 			# echo "#char => $_char"
 			function _result {
-				_previous_state="$1"
-				# echo "# => $_state_var_name = $_previous_state"
-				_create_string_var "$_state_var_name" < <(printf '%s' "$_previous_state")
+				local _new_state=$1
+				# echo "# => $_state_var_name = $_new_state"
+				_create_string_var "$_state_var_name" < <(printf '%s' "$_new_state")
 			}
 
 			if [ $_previous_state -eq $_STATE_NOT_READING ] || [ $_previous_state -eq $_STATE_END_READING ]; then
@@ -67,10 +82,8 @@ function _parse_border_description_syntax {
 		}
 
 		local _current_border=""
+		local _state_reading_border=$_STATE_NOT_READING
 		while [ $_qt_parsed_borders -lt $_qt_borders ] && read -n1 -r _borders_description_char; do
-
-			local _state_reading_border
-
 			_set_state "$_borders_description_char" _state_reading_border
 
 			if [ $_state_reading_border -eq $_STATE_READING ]; then
