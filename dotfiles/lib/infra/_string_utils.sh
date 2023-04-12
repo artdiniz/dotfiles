c87e25d7e2bacddccecf1f5811788c7d94228b47
Art Diniz Adam <1895150+artdiniz@users.noreply.github.com>
2023-04-12 18:11:08 -0300

    more generic terminal config

--- dotfiles/lib/infra/_string_utils.sh
+++ dotfiles/lib/infra/_string_utils.sh
@@ -19,3 +19,13 @@ function _string_starts_with {
         return 1    # $substring is not in $string
     fi
 }
+
+function _string_ends_with {
+    string="$1"
+    substring="$2"
+    if [ "${string%$substring}" != "$string" ]; then
+        return 0    # $substring is in $string
+    else
+        return 1    # $substring is not in $string
+    fi
+}
