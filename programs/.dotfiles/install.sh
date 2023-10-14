1102a3dc8b88aa169c403beb19bbc3d56a8d0a35
Art Diniz Adam <1895150+artdiniz@users.noreply.github.com>
2023-10-13 21:25:45 -0300

    TODO

--- programs/.dotfiles/install.sh
+++ programs/.dotfiles/install.sh
@@ -1,4 +1,6 @@
 mkdir -p "$HOME/bin"
+# TODO backup bash and zsh
+# TODO add backup bash and xsh files execution to shell env
 ~/dotfiles/.src/files_link.sh .dotfiles
 ~/dotfiles/.src/files_link.sh bash
 ~/dotfiles/.src/files_link.sh zsh
